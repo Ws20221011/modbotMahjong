fd1b31e3bf32ccb82e06e9ba0e68a44eed971e3b
Ws20221011 <115523953+Ws20221011@users.noreply.github.com>
2022-11-06 10:53:34 +0800

    Add files via upload

--- Mahjong.py
+++ Mahjong.py
@@ -63,10 +63,11 @@ def dd():
     data5_2="    日期:"
     data5_3="\n"
     data5_4="    時間:"
-    data5=[ data5_1+i[1]+"\n"+data5_2+i[2]+ data5_3+ data5_4+i[4]  for i in data4 ]
+    data5=[ data5_1+i[1]+"\n"+data5_2+i[2]+ data5_3+ data5_4+i[3]  for i in data4 ]
     
     data6=data5[0]+data5_3+data5[1]+data5_3+data5[2]
-    return  data6
+    return  data4
+
 
 
     
